0e7c46631e5d50be70083b9478cdd2540b6b86a2
JunaidCS032 <ansariahmedjunaid@gmail.com>
2015-08-09 18:34:15 +0530

    A helloGit project just to get started with Git
    This project is created to get acquainted with Git.

---
+++ helloGit.h
@@ -0,0 +1,28 @@
+/**
+    This is declaration of a HelloGit class in the helloGit.h header. 
+
+    The intention of this programs is to understand how to upload an
+    opensource project on GitHub
+
+    Author: Junaid Ahmed
+
+    Note: I might make complete no sense. Bare with me . . . .
+    
+ */
+
+#ifndef HELLOGIT_H
+#define HELLOGIT_H
+
+class HelloGitClass{
+
+
+    int _howMuchILoveGit;
+
+  public:
+
+    HelloGitClass();
+    void setLoveForGit( int love );
+    return getLoveForGit ();
+};
+
+#endif
